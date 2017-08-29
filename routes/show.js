e74377c307e6f17df035799e79ea8ff430154fdb
Rohan1194 <rohan_garg@stu.upes.ac.in>
2017-08-29 20:05:45 +0530

    TestCases

--- routes/show.js
+++ routes/show.js
@@ -7,20 +7,21 @@ mongoose.connect('mongodb://localhost:27017/bookstore');
 var bodyParser=require('body-parser');
 //var Data=require('../models/data');
 var router=express.Router();
+
 router.get('/show',function(req,res){
   dd.find({})
   .exec(function(err,books)
   {
   	res.json(books);
   });
-}); 
+});
+
 
 router.post('/add',function(req,res){
 dd.create(req.body).then(function(data){
 	res.send(data);
 });
 }); 
-
 
 router.delete('/remove',function(req,res){
 	console.log(req.body._id);
@@ -42,5 +43,23 @@ dd.findByIdAndUpdate({_id:req.params.id},req.body).then(function(){
  })
 })
   })
+
+/*Multiplication*/
+router.post('/multiply/:id1/:id2',(req,res)=>{
+  var a=req.params.id1;
+  console.log(a);
+  var b=req.params.id2;
+  var c=a * b;
+  console.log(c)
+  res.send(c.toString());
+})
+
+/*Addition*/
+router.post('/add/:id1/:id2',function(req,res){
+  var a=req.params.id1;
+  var b=req.params.id2;
+  var c= a + b;
+  res.send(c.toString());
+})
  
 module.exports= router;
---
+++ test/Apptest.js
@@ -0,0 +1,39 @@
+/*var assert = require('chai').assert;
+var app = require("../routes/new").test();
+
+describe('bookstore',function(){
+	it('app should return hello',function(){
+		assert.equal(app,"hello");
+	})
+})*/
+
+var should=require("chai").should(),
+supertest=require("supertest"),
+app=require("../routes/show");
+var url=supertest("http://localhost:3000");
+
+describe("testing first route",function(err){
+    it("should test multiplication",function(done)
+    {
+        url
+        .post("/user/multiply/3/4")
+        .end(function(err,res){
+              res.text.should.be.equal("12");
+              done();
+
+    });
+      });
+
+    it("should test Addition",function(done)
+    {
+        url
+        .post("/user/add/3/4")
+        .end(function(err,res){
+              res.text.should.be.equal("34");
+              done();
+
+    });
+      });
+
+});
+
